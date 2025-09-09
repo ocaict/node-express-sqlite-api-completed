de619e320203f47883638ceb6e8ad379f16c8bfb
Oluegwu Chigozie <ocaictcentre@gmail.com>
2025-09-09 03:19:04 +0100

    added nothing

--- routes/homeRoute.js
+++ routes/homeRoute.js
@@ -3,7 +3,10 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/", (req, res) => {
-  return res.json({ success: true, message: "Server Running..." });
+  return res.json({
+    success: true,
+    message: "Server Running...",
+  });
 });
 
 export default router;
