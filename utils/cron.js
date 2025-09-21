67744f20bb059b6055f0305fc12ff7a95dcda8b3
Oluegwu Chigozie <ocaictcentre@gmail.com>
2025-09-21 04:33:13 +0100

    Added Cron

--- utils/cron.js
+++ utils/cron.js
@@ -1,13 +1,15 @@
 import axios from "axios";
 import { CronJob } from "cron";
 
-// Run Cron Job Every 5 Minutes
+// Run Cron Job Every 14 Minutes
 const job = new CronJob(
-  "*/5 * * * *", // Runs every 5  minutes
+  "*/14 * * * *", // Runs every 14 minutes
   async () => {
     // Task to be executed
     try {
-      const response = await axios.get("http://localhost:3600"); // Replace with your API endpoint
+      const response = await axios.get(
+        "https://node-express-sqlite-api-completed.onrender.com/"
+      );
       console.log("Cron Job executed successfully:", response.data);
     } catch (error) {
       console.error("Error executing Cron Job:", error);
