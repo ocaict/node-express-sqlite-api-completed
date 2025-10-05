6b44f6437b8ff56c67a8fabe2933f34bc2a74836
Oluegwu Chigozie <ocaictcentre@gmail.com>
2025-10-06 00:34:36 +0100

    Added Cron with fetch

--- utils/cron.js
+++ utils/cron.js
@@ -1,15 +1,14 @@
 // Use fetch API to make HTTP requests
 import { CronJob } from "cron";
 
-// Run Cron Job Every 1 Minute
+// Run Cron Job Every 14 Minutes
 const job = new CronJob(
-  "*/1 * * * *", // Runs every 1 minute
+  "*/14 * * * *", // Runs every 14 minutes
   async () => {
     // Task to be executed
+    const baseUrl = process.env.RENDER_URL || "http://localhost:3600";
     try {
-      const response = await fetch(
-        "https://node-express-sqlite-api-completed.onrender.com/"
-      );
+      const response = await fetch(`${baseUrl}/`);
       const data = await response.json();
       console.log("Cron Job executed successfully:", data);
     } catch (error) {
